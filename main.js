13835e4c13c57206864c4feb7d8f1c4e2c199130
tdspriet <Thibaud.Despriet@UGent.be>
2024-06-24 00:26:29 +0200

    :sparkles: 3D Model!

--- main.js
+++ main.js
@@ -1,32 +1,47 @@
 import './style.css'
 import * as THREE from 'three'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 
 const scene = new THREE.Scene()
-
+scene.background = new THREE.Color(0x40495a);
+scene.rotation.y = -1.57
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-
+camera.position.setZ(20)
+camera.position.setY(2.5)
 const renderer = new THREE.WebGLRenderer({
     canvas: document.querySelector('#bg'),
     })
-
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight)
-
-camera.position.setZ(30)
-
 renderer.render(scene, camera)
 
-const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
-const material = new THREE.MeshBasicMaterial({color: 0xFF6347, wireframe: true})
-const torus = new THREE.Mesh(geometry, material)
+const loader = new GLTFLoader()
+loader.load( 'mech_hangar/scene.gltf', function ( gltf ) {
 
-scene.add(torus)
+    scene.add( gltf.scene );
 
+}, undefined, function ( error ) {
+
+    console.error( error );
+
+} );
+
+var clockwise = false
 function animate() {
     requestAnimationFrame(animate)
 
-    torus.rotation.x += 0.001
-    torus.rotation.y += 0.005
+    if(scene.rotation.y > -1) {
+        clockwise = false
+    }
+    if(scene.rotation.y < -2.25) {
+        clockwise = true
+    }
+
+    if(clockwise) {
+        scene.rotation.y += 0.0005
+    } else {
+        scene.rotation.y -= 0.0005
+    }
 
     renderer.render(scene, camera)
 }
